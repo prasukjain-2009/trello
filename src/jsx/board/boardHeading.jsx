8f94f0737d3361d8c93a34dbb8c3fd2058807d64
prasukjain-2009 <prasukjain.2009@gmail.com>
2018-10-07 20:34:54 +0530

    removed unused code

--- src/jsx/board/boardHeading.jsx
+++ src/jsx/board/boardHeading.jsx
@@ -13,10 +13,6 @@ class BoardHeading extends Component {
       <div className="board-heading my-4 row">
           <div
             className="bhead-body label col-lg-2, col-md-4 col-sm-10">{this.props.board}</div>
-          <input
-            value={this.state.board} onChange={evt => this.setState({ board: evt.target.value })}
-            className="bhead-body tf  col-lg-2, col-md-4 col-sm-12 col-xs-12" />
-        
       </div>
     )
   }
